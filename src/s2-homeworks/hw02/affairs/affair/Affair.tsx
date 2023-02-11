a317e176c55f6c3335167c71b58d4fbd8c1cb5ff
ElmivEn <elmiven@hotmail.com>
2023-02-12 01:25:43 +0300

    HW2 v1.2

--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -27,7 +27,7 @@ function Affair(props: AffairPropsType) {
         >
             <div id={'hw2-name-' + props.affair._id} className={nameClass}>
                 {/*создаёт студент*/}
-placeholder
+                {props.affair.name}
                 {/**/}
             </div>
             <div id={'hw2-priority-' + props.affair._id} hidden>
