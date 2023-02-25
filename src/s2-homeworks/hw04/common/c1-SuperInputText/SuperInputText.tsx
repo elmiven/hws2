5b26ace9623fdbffcb76e087b8a7ee2aba0502e9
ElmivEn <elmiven@hotmail.com>
2023-02-26 01:51:47 +0300

    HW4.4

--- src/s2-homeworks/hw04/common/c1-SuperInputText/SuperInputText.tsx
+++ src/s2-homeworks/hw04/common/c1-SuperInputText/SuperInputText.tsx
@@ -48,16 +48,16 @@ const SuperInputText: React.FC<SuperInputTextPropsType> = (
         onEnter() // то вызвать его
     }
 
-    // const finalSpanClassName = s.error
-    //     + (spanClassName ? ' ' + spanClassName : '')
+    const finalSpanClassName = s.error
+        + (spanClassName ? ' ' + spanClassName : '')
     
-    const finalSpanClassName = `${s.error} ${spanClassName}`
+    // const finalSpanClassName = `${s.error} ${spanClassName}`
     
-    //     const finalInputClassName = s.input
-    //     + (error ? ' ' + s.errorInput : ' ' + s.superInput)
-    //     + (className ? ' ' + s.className : '') // задача на смешивание классов
+        const finalInputClassName = s.input
+        + (error ? ' ' + s.errorInput : ' ' + s.superInput)
+        + (className ? ' ' + s.className : '') // задача на смешивание классов
 
-    const finalInputClassName = `${s.input} ${error ? s.errorInput : s.superInput} ${className}`
+    // const finalInputClassName = `${s.input} ${error ? s.errorInput : s.superInput} ${className}`
 
     return (
         <div className={s.inputWrapper}>
